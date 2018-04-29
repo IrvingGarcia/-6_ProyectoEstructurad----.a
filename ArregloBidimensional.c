60ff4e263404478b0b2ab57ce7094d9e4dc82f91
IrvingGarcia <irvin.16@outlook.es>
2018-04-28 20:43:59 -0500

    Actualizando arreglobidimensional.c

--- ArregloBidimensional.c
+++ ArregloBidimensional.c
@@ -17,23 +17,22 @@
 int main(void)
 {
      int array[ROW][COL];
+     float arreglo[ROW][COL];
 
      // Inicializar el array con valores aleatorios 1 - 100
      for (int i=0; ; i < ROW; i++)
      {
           for (int j=0; j<COL; J++)
           {
-              // Asignación de datod al arreglo
+              // Asignación de datos al arreglo
               // Utilizar la fumción de Scanf() para solicitar datos al usuario
 
               // Utilizar la función rand() para solicitar datos al usuario
-              // La función se encuentra definida dentro de la biblioteca stdlib.h
-
+              // La función se encuentra definida dentro de la biblioteca stdlib.
               array[i][j]= rand() % (MAX +1);
 
-
               // Tarea: Generar valores aleatorios de tipo flotante entre 0 - 1000
-
+              arreglo[i][j]= rand() %  (MAX +1);
      }
      printf("\xlb[32m Datos aleatororios de la matriz: array[%d][%d]\n", ROW, COL);
      printf("\xlb[31m Otro mensaje\n");
@@ -50,5 +49,19 @@ int main(void)
           printf("%s\n", );
      }
      printf("%s\n", );
+
+     printf("{\n");
+     for (int i=0; ; i < ROW; i++)
+     {
+          printf("{\n");
+
+          for (int j=0; j<COL; J++)
+          {
+              printf("%f", arreglo[i][j]);
+          }
+          printf("%s\n", );
+     }
+     printf("%s\n", );
+
      return 0;
 }
